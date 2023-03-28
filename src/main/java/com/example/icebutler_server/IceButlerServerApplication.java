0faa1595e30996c96e9afea176ba9ebf10a58af7
kingchan223 <kokiyo97@naver.com>
2023-03-28 19:51:03 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/icebutler_server/IceButlerServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.icebutler_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceButlerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IceButlerServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/icebutler_server/IceButlerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.icebutler_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IceButlerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

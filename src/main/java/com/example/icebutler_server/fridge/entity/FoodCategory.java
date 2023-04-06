6a89c259b3e7c1a8202c772779d6916bb9777f12
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-04-06 11:49:21 +0900

    feat: food, recipe, report 카테고리 추가

--- src/main/java/com/example/icebutler_server/fridge/entity/FoodCategory.java
+++ src/main/java/com/example/icebutler_server/fridge/entity/FoodCategory.java
@@ -4,5 +4,21 @@ import lombok.Getter;
 
 @Getter
 public enum FoodCategory{
+  MEAT("육류"),
+  FRUIT("과일"),
+  VEGETABLE("채소"),
+  BEVERAGE("음료"),
+  AQUATIC_PRODUCTS("수산물"),
+  BANCHAN("반찬"),
+  SNACK("간식"),
+  CONDIMENT("조미료"),
+  PROCESSED_FOOD("가공식품"),
+  ETC("기타");
 
+  private final String category;
+
+  private FoodCategory(String category) {
+    this.category = category;
+  }
 }
+

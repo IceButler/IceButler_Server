af9eab378a5561f11c260ac06c257487e5c2717f
sojungpp <sophie2458@naver.com>
2023-04-08 03:05:41 +0900

    #1 feat: 장바구니 식품 조회 API response 추가

--- src/main/java/com/example/icebutler_server/food/dto/response/FoodResponse.java
+++ src/main/java/com/example/icebutler_server/food/dto/response/FoodResponse.java
@@ -10,12 +10,15 @@ public class FoodResponse {
 
     private Long foodIdx;
     private String foodName;
+    private String foodIconName;
     private String foodCategory;
 
     public static FoodResponse toDto(Food food) {
         FoodResponse foodResponse = new FoodResponse();
         foodResponse.foodIdx = food.getFoodIdx();
         foodResponse.foodName = food.getFoodName();
+        foodResponse.foodIconName = food.getFoodIconName();
+        foodResponse.foodCategory = food.getFoodCategory().getName();
         return foodResponse;
     }
 }

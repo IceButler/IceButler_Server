ceaccb4dfd670d5c95f8cdd72af4a169cd9851ec
chaerlo127 <monynony0203@gmail.com>
2023-04-09 03:26:44 +0900

    #5 interface method 추가

--- src/main/java/com/example/icebutler_server/fridge/service/FridgeService.java
+++ src/main/java/com/example/icebutler_server/fridge/service/FridgeService.java
@@ -2,11 +2,14 @@ package com.example.icebutler_server.fridge.service;
 
 import com.example.icebutler_server.fridge.dto.fridge.request.FridgeModifyReq;
 import com.example.icebutler_server.food.entity.Food;
+import com.example.icebutler_server.fridge.dto.fridge.response.FridgeMainRes;
 import com.example.icebutler_server.global.dto.response.ResponseCustom;
 
 import java.util.List;
 
 public interface FridgeService {
+
+  public FridgeMainRes getFoods(Long fridgeIdx, Long userIdx, String category);
   public void modifyFridge(Long fridgeIdx, FridgeModifyReq updateFridgeReq, Long userIdx);
 
   public ResponseCustom<Long> removeFridge(Long fridgeIdx, Long userIdx);

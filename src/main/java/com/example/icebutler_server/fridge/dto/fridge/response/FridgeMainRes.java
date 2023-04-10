023a64f8e32bc5219f6544e253a9c22c3ebeb5bd
chaerlo127 <monynony0203@gmail.com>
2023-04-10 18:17:33 +0900

    #5 fix: response toDto 생성

--- src/main/java/com/example/icebutler_server/fridge/dto/fridge/response/FridgeMainRes.java
+++ src/main/java/com/example/icebutler_server/fridge/dto/fridge/response/FridgeMainRes.java
@@ -1,16 +1,23 @@
 package com.example.icebutler_server.fridge.dto.fridge.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.example.icebutler_server.fridge.dto.fridge.assembler.FridgeUtils;
+import com.example.icebutler_server.fridge.entity.fridge.FridgeFood;
+import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class FridgeMainRes {
-  List<FridgeFoodsRes> foodList;
+  List<FridgeFoodsRes> foodList = new ArrayList<>();
+
+  public static FridgeMainRes toDto(List<FridgeFood> fridgeFoods) {
+    return new FridgeMainRes(fridgeFoods.stream()
+            .map(ff -> new FridgeFoodsRes(ff.getFridgeFoodIdx(), ff.getFood().getFoodName(), ff.getFood().getFoodIconName(), FridgeUtils.calShelfLife(ff.getShelfLife())))
+            .collect(Collectors.toList()));
+  }
 }

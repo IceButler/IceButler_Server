5228c899abf138f2bb7650ba4423ad79760b09b0
sojungpp <sophie2458@naver.com>
2023-04-13 03:30:38 +0900

    #78 feat: 공용 장바구니 식품 조회 API 추가

--- src/main/java/com/example/icebutler_server/cart/controller/MultiCartController.java
+++ src/main/java/com/example/icebutler_server/cart/controller/MultiCartController.java
@@ -1,6 +1,12 @@
 package com.example.icebutler_server.cart.controller;
 
+import com.example.icebutler_server.cart.service.MultiCartServiceImpl;
+import com.example.icebutler_server.global.dto.response.ResponseCustom;
+import com.example.icebutler_server.global.resolver.IsLogin;
+import com.example.icebutler_server.global.resolver.LoginStatus;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/multiCarts")
 @RestController
 public class MultiCartController {
+
+    private final MultiCartServiceImpl cartService;
+
+    // 공용 장바구니 식품 조회
+    @GetMapping("/{multiFridgeIdx}/foods")
+    public ResponseCustom<?> getCartFood( @PathVariable Long multiFridgeIdx,
+                                          @IsLogin LoginStatus loginStatus) {
+        return cartService.getFoodsFromCart(multiFridgeIdx, loginStatus.getUserIdx());
+    }
 }
--- src/main/java/com/example/icebutler_server/cart/service/MultiCartServiceImpl.java
+++ src/main/java/com/example/icebutler_server/cart/service/MultiCartServiceImpl.java
@@ -3,19 +3,58 @@ package com.example.icebutler_server.cart.service;
 import com.example.icebutler_server.cart.dto.cart.request.AddFoodToCartRequest;
 import com.example.icebutler_server.cart.dto.cart.request.RemoveFoodFromCartRequest;
 import com.example.icebutler_server.cart.dto.cart.response.CartResponse;
+import com.example.icebutler_server.cart.entity.multiCart.MultiCart;
+import com.example.icebutler_server.cart.entity.multiCart.MultiCartFood;
+import com.example.icebutler_server.cart.exception.CartNotFoundException;
+import com.example.icebutler_server.cart.repository.multiCart.MultiCartFoodRepository;
+import com.example.icebutler_server.cart.repository.multiCart.MultiCartRepository;
+import com.example.icebutler_server.food.entity.FoodCategory;
+import com.example.icebutler_server.fridge.entity.multiFridge.MultiFridge;
+import com.example.icebutler_server.fridge.entity.multiFridge.MultiFridgeUser;
+import com.example.icebutler_server.fridge.exception.FridgeNotFoundException;
+import com.example.icebutler_server.fridge.exception.FridgeUserNotFoundException;
+import com.example.icebutler_server.fridge.repository.multiFridge.MultiFridgeRepository;
+import com.example.icebutler_server.fridge.repository.multiFridge.MultiFridgeUserRepository;
 import com.example.icebutler_server.global.dto.response.ResponseCustom;
+import com.example.icebutler_server.user.entity.User;
+import com.example.icebutler_server.user.exception.UserNotFoundException;
+import com.example.icebutler_server.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Transactional(readOnly = true)
 @Service
 @RequiredArgsConstructor
 public class MultiCartServiceImpl implements CartService {
 
+    private final UserRepository userRepository;
+    private final MultiFridgeRepository multiFridgeRepository;
+    private final MultiFridgeUserRepository multiFridgeUserRepository;
+    private final MultiCartFoodRepository multiCartFoodRepository;
+    private final MultiCartRepository multiCartRepository;
+
     @Override
-    public ResponseCustom<CartResponse> getFoodsFromCart(Long cartIdx, Long userIdx) {
-        return null;
+    public ResponseCustom<?> getFoodsFromCart(Long fridgeIdx, Long userIdx) {
+        User user = userRepository.findByUserIdxAndIsEnable(userIdx, true).orElseThrow(UserNotFoundException::new);
+        MultiFridge fridge = multiFridgeRepository.findByMultiFridgeIdxAndIsEnable(fridgeIdx, true).orElseThrow(FridgeNotFoundException::new);
+        MultiFridgeUser fridgeUser = multiFridgeUserRepository.findByMultiFridgeAndUserAndIsEnable(fridge, user, true).orElseThrow(FridgeUserNotFoundException::new);
+        MultiCart cart = multiCartRepository.findByMultiFridgeUserAndIsEnable(fridgeUser, true).orElseThrow(CartNotFoundException::new);
+
+        List<CartResponse> cartResponses = new ArrayList<>();
+        for (FoodCategory category : FoodCategory.values()) {
+            List<MultiCartFood> cartFoods = multiCartFoodRepository.findByMultiCartAndFood_FoodCategoryAndIsEnableOrderByCreatedAt(cart, category, true);
+            // 카테고리별 음식이 있는 경우만 응답
+            if(cartFoods.isEmpty()) continue;
+            CartResponse cartResponse = CartResponse.doMultiDto(cartFoods, category);
+            cartResponses.add(cartResponse);
+        }
+        // 카테고리별 음식 개수 내림차순 정렬
+        cartResponses.sort((cf1, cf2) -> cf2.getCartFoods().size() - cf1.getCartFoods().size());
+        return ResponseCustom.OK(cartResponses);
     }
 
     @Override

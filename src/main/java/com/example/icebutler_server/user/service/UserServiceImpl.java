62adde4cea0cbdf9132a0a89496f1854a95e1d1e
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-04-16 21:31:38 +0900

    #80 fix: 마이프로필 조회 로직 수정

--- src/main/java/com/example/icebutler_server/user/service/UserServiceImpl.java
+++ src/main/java/com/example/icebutler_server/user/service/UserServiceImpl.java
@@ -83,11 +83,13 @@ public class UserServiceImpl implements UserService {
   public MyProfileRes myProfile(Long userIdx) {
     User user = userRepository.findByUserIdxAndIsEnable(userIdx,true).orElseThrow(UserNotFoundException::new);
 
-    return MyProfileRes.builder()
-            .userIdx(user.getUserIdx())
-            .nickName(user.getNickname())
-            .profileImage(user.getProfileImage())
-            .build();
+//    return MyProfileRes.builder()
+//            .userIdx(user.getUserIdx())
+//            .nickName(user.getNickname())
+//            .email(user.getEmail())
+//            .profileImage(user.getProfileImage())
+//            .build();
+    return new MyProfileRes(user);
   }
 
 }

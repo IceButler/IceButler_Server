a8bfb18ceef7848c0e34558b49cb0ef6c64ebde6
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-04-21 16:01:09 +0900

    #5 refactor: 오타수정

--- src/main/java/com/example/icebutler_server/fridge/exception/PermissionDeniedException.java
+++ src/main/java/com/example/icebutler_server/fridge/exception/PermissionDeniedException.java
@@ -1,5 +1,5 @@
 package com.example.icebutler_server.fridge.exception;
 
 public class PermissionDeniedException extends RuntimeException {
-  public PermissionDeniedException() {super("올바르지 않은 접근 권환입니다.");}
+  public PermissionDeniedException() {super("올바르지 않은 접근 권한입니다.");}
 }

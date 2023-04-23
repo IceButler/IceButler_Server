61b7da9c5fa8d72a810ea67e8eec10cd6810977e
psyeon1120 <psycsh@naver.com>
2023-04-23 23:48:37 +0900

    #33 refactor: 디데이 반환타입 및 계산법 수정

--- src/main/java/com/example/icebutler_server/fridge/dto/fridge/assembler/FridgeUtils.java
+++ src/main/java/com/example/icebutler_server/fridge/dto/fridge/assembler/FridgeUtils.java
@@ -4,13 +4,8 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public abstract class FridgeUtils {
-    public static String calShelfLife(LocalDate shelfLife) {
-        long day = Math.abs(ChronoUnit.DAYS.between(LocalDate.now(), shelfLife));
-        String mark;
-        if(shelfLife.isAfter(LocalDate.now())) mark = "-";
-        else mark = "+";
-
-        return "D"+mark+day;
+    public static int calShelfLife(LocalDate shelfLife) {
+        return (int) (-1 * ChronoUnit.DAYS.between(LocalDate.now(), shelfLife));
     }
 
     public static double calPercentage(int val, int sum) {

9b0c5d2bca514c84dc42a0d7443a328abb4f09e5
sojungpp <sophie2458@naver.com>
2023-04-24 01:06:03 +0900

    #1 #78 fix: food image res 변경

--- src/main/java/com/example/icebutler_server/food/dto/assembler/FoodAssembler.java
+++ src/main/java/com/example/icebutler_server/food/dto/assembler/FoodAssembler.java
@@ -5,10 +5,12 @@ import com.example.icebutler_server.food.dto.response.FoodRes;
 import com.example.icebutler_server.food.entity.Food;
 import com.example.icebutler_server.food.entity.FoodCategory;
 import com.example.icebutler_server.fridge.dto.fridge.request.FridgeFoodReq;
+import com.example.icebutler_server.global.util.AwsS3ImageUrlUtil;
 import com.example.icebutler_server.global.util.Constant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import static com.example.icebutler_server.global.util.Constant.Food.FOOD;
 import static com.example.icebutler_server.global.util.Constant.Food.ICON_EXTENSION;
 
 @Component
@@ -33,9 +35,10 @@ public class FoodAssembler {
   }
 
   public Food toEntity(AddFoodRequest request) {
+    String foodImageKey = FOOD + request.getFoodCategory()+ ICON_EXTENSION;
     return Food.builder()
             .foodName(request.getFoodName())
-            .foodIconName(request.getFoodCategory()+ ICON_EXTENSION)
+            .foodIconName(AwsS3ImageUrlUtil.toUrl(foodImageKey))
             .foodCategory(FoodCategory.getFoodCategoryByName(request.getFoodCategory()))
             .build();
   }
--- src/main/java/com/example/icebutler_server/global/util/Constant.java
+++ src/main/java/com/example/icebutler_server/global/util/Constant.java
@@ -4,7 +4,9 @@ public final class Constant {
   public static final String FRIDGE = "fridge";
   public static final String MULTI_FRIDGE = "multi";
   public static final String COMMA = ",";
+  public static final String SLASH = "/";
   public static class Food {
+    public static final String FOOD = "food";
     public static final String ICON_EXTENSION = ".png";
   }
 }

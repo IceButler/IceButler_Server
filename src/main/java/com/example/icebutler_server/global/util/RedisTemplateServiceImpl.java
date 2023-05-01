4050032fc72ae9a3225da60b92bca55c5ba257df
이승학 <iseunghag@172.20.nate.com>
2023-05-01 18:22:37 +0900

    레디스 메모리로 임시변경

--- src/main/java/com/example/icebutler_server/global/util/RedisTemplateServiceImpl.java
+++ src/main/java/com/example/icebutler_server/global/util/RedisTemplateServiceImpl.java
@@ -1,32 +1,32 @@
-package com.example.icebutler_server.global.util;
-
-import com.sun.istack.NotNull;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@RequiredArgsConstructor
-@Service
-public class RedisTemplateServiceImpl implements RedisTemplateService {
-
-    private final RedisTemplate<Long, String> redisTemplate;
-
-    public void deleteUserRefreshToken(@NotNull Long userIdx){
-        ValueOperations<Long, String> valueOperations = redisTemplate.opsForValue();
-        valueOperations.getAndDelete(userIdx);
-    }
-
-//    @Nullable
-    @Transactional(readOnly = true)
-    public String getUserRefreshToken(@NotNull Long userIdx) {
-        ValueOperations<Long, String> valueOperations = redisTemplate.opsForValue();
-        return valueOperations.get(userIdx);
-    }
-
-    public void setUserRefreshToken(@NotNull Long userIdx, String refreshToken) {
-        ValueOperations<Long, String> valueOperations = redisTemplate.opsForValue();
-        valueOperations.set(userIdx, refreshToken);
-    }
-}
+//package com.example.icebutler_server.global.util;
+//
+//import com.sun.istack.NotNull;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.core.ValueOperations;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@RequiredArgsConstructor
+//@Service
+//public class RedisTemplateServiceImpl implements RedisTemplateService {
+//
+//    private final RedisTemplate<Long, String> redisTemplate;
+//
+//    public void deleteUserRefreshToken(@NotNull Long userIdx){
+//        ValueOperations<Long, String> valueOperations = redisTemplate.opsForValue();
+//        valueOperations.getAndDelete(userIdx);
+//    }
+//
+////    @Nullable
+//    @Transactional(readOnly = true)
+//    public String getUserRefreshToken(@NotNull Long userIdx) {
+//        ValueOperations<Long, String> valueOperations = redisTemplate.opsForValue();
+//        return valueOperations.get(userIdx);
+//    }
+//
+//    public void setUserRefreshToken(@NotNull Long userIdx, String refreshToken) {
+//        ValueOperations<Long, String> valueOperations = redisTemplate.opsForValue();
+//        valueOperations.set(userIdx, refreshToken);
+//    }
+//}

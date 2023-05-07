b6df37c990bf449489d47b6bb5dc185bf422b026
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-05-07 18:47:20 +0900

    #5 refactor: 냉장고 조회 응답값 userIdx 추가

--- src/main/java/com/example/icebutler_server/fridge/dto/fridge/response/FridgeUserRes.java
+++ src/main/java/com/example/icebutler_server/fridge/dto/fridge/response/FridgeUserRes.java
@@ -13,12 +13,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class FridgeUserRes {
+    private Long userIdx;
     private String nickname;
     private FridgeRole role;
     private String profileImgUrl;
 
     public static FridgeUserRes toDto(User user, FridgeRole role) {
         FridgeUserRes fridgeUserRes = new FridgeUserRes();
+        fridgeUserRes.userIdx = user.getUserIdx();
         fridgeUserRes.nickname = user.getNickname();
         fridgeUserRes.role = role;
         fridgeUserRes.profileImgUrl = AwsS3ImageUrlUtil.toUrl(user.getProfileImgKey());

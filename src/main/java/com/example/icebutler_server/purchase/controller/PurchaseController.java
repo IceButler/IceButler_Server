371918da23c95bfb3aa6ab018d81ee1fee940121
psyeon1120 <psycsh@naver.com>
2023-05-08 02:21:14 +0900

    #196 refactor: RequestMapping path 변경

--- src/main/java/com/example/icebutler_server/purchase/controller/PurchaseController.java
+++ src/main/java/com/example/icebutler_server/purchase/controller/PurchaseController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/purchase")
+@RequestMapping(value = "/purchases")
 @RequiredArgsConstructor
 public class PurchaseController {
 }

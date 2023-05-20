8c8bcf26471e1cff23c104552afb2c076cd20d98
kingchan223 <kokiyo97@naver.com>
2023-05-20 22:47:39 +0900

    #224 admin exception controller 추가

---
+++ src/main/java/com/example/icebutler_server/admin/controller/AdminExceptionController.java
@@ -0,0 +1,34 @@
+package com.example.icebutler_server.admin.controller;
+
+import com.example.icebutler_server.admin.exception.AdminAnnotationIsNowhereException;
+import com.example.icebutler_server.admin.exception.AdminNotFoundException;
+import com.example.icebutler_server.admin.exception.FoodNotFoundException;
+import com.example.icebutler_server.admin.exception.PasswordNotMatchException;
+import com.example.icebutler_server.cart.exception.CartNotFoundException;
+import com.example.icebutler_server.global.dto.response.ResponseCustom;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class AdminExceptionController {
+
+    @ExceptionHandler(AdminNotFoundException.class)
+    public ResponseCustom<Void> adminNotFoundException(AdminNotFoundException e){
+        log.error(e.getMessage());
+        return ResponseCustom.NOT_FOUND(e.getMessage());
+    }
+
+    @ExceptionHandler(FoodNotFoundException.class)
+    public ResponseCustom<Void> foodNotFoundException(FoodNotFoundException e){
+        log.error(e.getMessage());
+        return ResponseCustom.NOT_FOUND(e.getMessage());
+    }
+
+    @ExceptionHandler(PasswordNotMatchException.class)
+    public ResponseCustom<Void> passwordNotMatchException(PasswordNotMatchException e){
+        log.error(e.getMessage());
+        return ResponseCustom.BAD_REQUEST(e.getMessage());
+    }
+}

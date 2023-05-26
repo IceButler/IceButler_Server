2d21bc03822fd6e0d9148494732fe95856295a9f
chaerlo127 <monynony0203@gmail.com>
2023-05-26 10:02:26 +0900

    #224 fix: 관리자 회원가입 시 InvalidDefinitionException 에러 해결

--- src/main/java/com/example/icebutler_server/global/dto/response/ResponseCustom.java
+++ src/main/java/com/example/icebutler_server/global/dto/response/ResponseCustom.java
@@ -1,9 +1,6 @@
 package com.example.icebutler_server.global.dto.response;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.Nullable;
 
@@ -12,7 +9,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @ToString
-@Builder
+@NoArgsConstructor
 public class ResponseCustom<T>{
 
     private T data;
@@ -20,6 +17,15 @@ public class ResponseCustom<T>{
     private HttpStatus status;
     private String description;
     private int statusCode;
+
+    @Builder
+    public ResponseCustom(T data, LocalDateTime transaction_time, HttpStatus status, String description, int statusCode) {
+        this.data = data;
+        this.transaction_time = transaction_time;
+        this.status = status;
+        this.description = description;
+        this.statusCode = statusCode;
+    }
 
     // OK
     public static <T> ResponseCustom<T> CREATED(@Nullable T data) {

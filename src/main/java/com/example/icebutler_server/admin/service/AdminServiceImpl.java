0c7d784d8f8e45448101ca185a8e1a3b7abc9c6f
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-06-04 21:43:20 +0900

    #195 feat: 주석제거

--- src/main/java/com/example/icebutler_server/admin/service/AdminServiceImpl.java
+++ src/main/java/com/example/icebutler_server/admin/service/AdminServiceImpl.java
@@ -107,7 +107,6 @@ public class AdminServiceImpl implements AdminService {
             return searchFoods;
         }
 
-//        Page<Food> all = foodRepository.findByIsEnableOrderByCreatedAt(true, pageable);
         Page<Food> all = foodRepository.findByIsEnableOrderByUpdateAtDesc(true, pageable);
         searchFoods = all.map(SearchFoodsResponse::toDto);
         return searchFoods;

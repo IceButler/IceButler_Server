493409841f63d4da16a8a7e29589dc536012aec8
kingchan223 <kokiyo97@naver.com>
2023-06-07 14:36:01 +0900

    admin withdraw컨트롤러에 토큰 받도록 수정

--- src/main/java/com/example/icebutler_server/global/feign/feignClient/RecipeServerClient.java
+++ src/main/java/com/example/icebutler_server/global/feign/feignClient/RecipeServerClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @Component
-@FeignClient(name="recipe-server", url = "${server.recipe.local-url}")
+@FeignClient(name="recipe-server", url = "${server.recipe.url}")
 public interface RecipeServerClient {
     @PostMapping("/users")
     void addUser(@RequestBody UserReq userReq);

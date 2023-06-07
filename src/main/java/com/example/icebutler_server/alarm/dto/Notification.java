1606afe6066fc0189b249c19d5744315fcae0ede
chaerlo127 <monynony0203@gmail.com>
2023-06-07 18:00:44 +0900

    #201 fix: 알림에러해결

--- src/main/java/com/example/icebutler_server/alarm/dto/Notification.java
+++ src/main/java/com/example/icebutler_server/alarm/dto/Notification.java
@@ -11,14 +11,14 @@ public class Notification {
     private String title;
     private String body;
     private String image;
-    private String sound;
+//    private String sound;
 
     public static Notification toEntity(String title, String body) {
         return Notification.builder()
                 .title(title)
                 .body(body)
                 .image(null)
-                .sound("default")
+//                .sound("default")
                 .build();
     }
 }

0fc678ba054a5724971e37486cd3111c0a1c5b83
이승학 <iseunghag@iseunghag-ui-MacBookAir.local>
2023-06-26 02:52:59 +0900

    #303 test:장바구니 CRUD,유저 프로필 편집 테스트 코드 작성

--- src/main/java/com/example/icebutler_server/user/dto/request/PatchProfileReq.java
+++ src/main/java/com/example/icebutler_server/user/dto/request/PatchProfileReq.java
@@ -1,8 +1,10 @@
 package com.example.icebutler_server.user.dto.request;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class PatchProfileReq {
   private String nickname;
   private String profileImgKey;

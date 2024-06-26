deb479d5e5f7d4b695efbff67725584e300884a5
sojungpp <sophie2458@naver.com>
2024-06-26 22:11:54 +0900

    #309 fix: 이전 스웨거 임의 설정 삭제

--- src/main/java/com/example/icebutler_server/user/controller/UserAuthController.java
+++ src/main/java/com/example/icebutler_server/user/controller/UserAuthController.java
@@ -5,8 +5,6 @@ import com.example.icebutler_server.global.resolver.Auth;
 import com.example.icebutler_server.global.resolver.IsLogin;
 import com.example.icebutler_server.global.resolver.LoginStatus;
 import com.example.icebutler_server.global.util.TokenUtils;
-import com.example.icebutler_server.user.dto.request.UserAuthTokenReq;
-import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 @RestController
-@Api(tags = "User Auth 관련 API")
 public class UserAuthController {
 
   private final TokenUtils tokenUtils;

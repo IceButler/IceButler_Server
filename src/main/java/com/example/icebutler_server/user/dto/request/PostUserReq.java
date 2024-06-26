963c3eaca3f2eecc0c12301084c65f026f8ad419
sojungpp <sophie2458@naver.com>
2024-06-26 23:23:53 +0900

    #309 feat: 회원가입 API 스웨거 추가

--- src/main/java/com/example/icebutler_server/user/dto/request/PostUserReq.java
+++ src/main/java/com/example/icebutler_server/user/dto/request/PostUserReq.java
@@ -1,12 +1,19 @@
 package com.example.icebutler_server.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
+@Schema(name = "PostUserReq", description = "유저 회원가입 요청 정보")
 @Getter
 public class PostUserReq {
+  @Schema(description = "프로바이더", allowableValues = {"카카오", "애플"}, example = "카카오")
   private String provider;
+  @Schema(description = "이메일", example = "abd@email.com")
   private String email;
+  @Schema(description = "닉네임")
   private String nickname;
+  @Schema(description = "프로필 이미지 키")
   private String profileImgKey;
+  @Schema(description = "FCM 토큰")
   private String fcmToken;
 }
--- src/main/java/com/example/icebutler_server/user/dto/response/PostUserRes.java
+++ src/main/java/com/example/icebutler_server/user/dto/response/PostUserRes.java
@@ -1,13 +1,17 @@
 package com.example.icebutler_server.user.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 import static com.example.icebutler_server.global.util.Constant.COMMA;
 
+@Schema(name = "PostUserRes", description = "유저 회원가입 정보")
 @Data
 public class PostUserRes {
+    @Schema(description = "액세스 토큰", example = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBQ0NFU1MtVE9LRU4iLCJpYXQiOjE3MTk0MTAyMzYsInVzZXJJZHgiOjEsIm5pY2tuYW1lIjoi7IaM7KCVIiwiZXhwIjoxNzIwNjE5ODM2fQ.X8FMuGXaI0crDWgyyXSb6IfuaSLZ8NMiRCSfc-NIryc")
     private String accessToken;
+    @Schema(description = "리프레쉬 토큰", example = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBQ0NFU1MtVE9LRU4iLCJpYXQiOjE3MTk0MTAyMzYsInVzZXJJZHgiOjEsIm5pY2tuYW1lIjoi7IaM7KCVIiwiZXhwIjaxNzIwNjE5ODM2fQ.X8FMuGX_I0crDWgyyXSb6IfuaSLZ8NMiRCSfc-NIryc")
     private String refreshToken;
 
     @Builder

efc15cc85532a3765e6a687f7ff5e60d115a8e17
psyeon1120 <psycsh@naver.com>
2024-06-29 18:53:11 +0900

    #313 docs: 식품 부분 스웨거 작성

--- src/main/java/com/example/icebutler_server/food/dto/response/BarcodeFoodRes.java
+++ src/main/java/com/example/icebutler_server/food/dto/response/BarcodeFoodRes.java
@@ -1,5 +1,6 @@
 package com.example.icebutler_server.food.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,8 +10,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Schema(name = "BarcodeFoodRes", description = "식품 바코드 검색 정보")
 public class BarcodeFoodRes {
-
+    @Schema(name = "식품명")
     private String foodDetailName;
 
     public static BarcodeFoodRes toDto(String foodDetailName) {

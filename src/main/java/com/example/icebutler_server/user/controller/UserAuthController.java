c978afbbdab5df280d289740cbefb33ff8135c4a
sojungpp <sophie2458@naver.com>
2024-06-30 23:10:52 +0900

    #312 refactor: 유저 인증/미인증 API 컨트롤러 분리

--- src/main/java/com/example/icebutler_server/user/controller/UserAuthController.java
+++ src/main/java/com/example/icebutler_server/user/controller/UserAuthController.java
@@ -5,16 +5,26 @@ import com.example.icebutler_server.global.resolver.Auth;
 import com.example.icebutler_server.global.resolver.IsLogin;
 import com.example.icebutler_server.global.resolver.LoginStatus;
 import com.example.icebutler_server.global.util.TokenUtils;
+import com.example.icebutler_server.user.dto.request.PatchProfileReq;
+import com.example.icebutler_server.user.dto.response.MyProfileRes;
+import com.example.icebutler_server.user.service.UserService;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 
 @RequiredArgsConstructor
-@RequestMapping("/auth")
+@RequestMapping(value = "/users")
 @RestController
+@Tag(name = "User", description = "유저 API (인증 필요)")
+ @SecurityRequirement(name = "Bearer")
 public class UserAuthController {
 
   private final TokenUtils tokenUtils;
+  private final UserService userService;
 
   @Auth
   @GetMapping("/renew")
@@ -22,5 +32,48 @@ public class UserAuthController {
     return ResponseCustom.OK(tokenUtils.accessExpiration(loginStatus.getUserIdx()));
   }
 
+  @Auth
+  @ResponseBody
+  @PatchMapping("/profile")
+  public ResponseCustom<?> modifyProfile(@RequestBody PatchProfileReq patchProfileReq,
+                                         @Parameter(hidden = true) @IsLogin LoginStatus loginStatus) {
+    userService.modifyProfile(loginStatus.getUserIdx(), patchProfileReq);
+    return ResponseCustom.OK();
+  }
+
+  @Auth
+  @DeleteMapping("/delete")
+  public ResponseCustom<?> deleteUser(
+          @IsLogin LoginStatus loginStatus
+  ) {
+    userService.deleteUser(loginStatus.getUserIdx());
+    return ResponseCustom.OK();
+  }
+
+  @Auth
+  @PostMapping("/logout")
+  public ResponseCustom<?> logout(
+          @IsLogin LoginStatus loginStatus
+  ) {
+    userService.logout(loginStatus.getUserIdx());
+    return ResponseCustom.OK();
+  }
+
+  @Auth
+  @GetMapping("")
+  public ResponseCustom<MyProfileRes> profile(
+          @IsLogin LoginStatus loginStatus
+  ) {
+    return ResponseCustom.OK(userService.checkProfile(loginStatus.getUserIdx()));
+  }
+
+  @Auth
+  @GetMapping("/notification")
+  public ResponseCustom<?> getUserNotification(
+          @IsLogin LoginStatus loginStatus,
+          Pageable pageable
+  ) {
+    return ResponseCustom.OK(userService.getUserNotification(loginStatus.getUserIdx(), pageable));
+  }
 
 }
--- src/main/java/com/example/icebutler_server/user/controller/UserController.java
+++ src/main/java/com/example/icebutler_server/user/controller/UserController.java
@@ -2,14 +2,9 @@ package com.example.icebutler_server.user.controller;
 
 import com.example.icebutler_server.global.dto.response.ResponseCustom;
 import com.example.icebutler_server.global.dto.response.SwaggerApiSuccess;
-import com.example.icebutler_server.global.resolver.IsLogin;
-import com.example.icebutler_server.global.resolver.LoginStatus;
 import com.example.icebutler_server.user.dto.LoginUserReq;
-import com.example.icebutler_server.user.dto.request.PatchProfileReq;
 import com.example.icebutler_server.user.dto.request.PostNicknameReq;
 import com.example.icebutler_server.user.dto.request.PostUserReq;
-import com.example.icebutler_server.user.dto.response.MyNotificationRes;
-import com.example.icebutler_server.user.dto.response.MyProfileRes;
 import com.example.icebutler_server.user.dto.response.PostUserRes;
 import com.example.icebutler_server.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,19 +13,14 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
-import com.example.icebutler_server.global.resolver.Auth;
-import org.springframework.data.domain.Pageable;
 
 @RestController
 @RequestMapping(value = "/users")
 @RequiredArgsConstructor
-@Tag(name = "User", description = "유저 API")
-// @SecurityRequirement(name = "Bearer")
+@Tag(name = "User", description = "유저 API (인증 불필요)")
 public class UserController {
 
   private final UserService userService;
@@ -51,65 +41,17 @@ public class UserController {
     return ResponseCustom.OK(userService.login(loginUserReq));
   }
 
-  @Auth
-  @ResponseBody
-  @PatchMapping("/profile")
-  public ResponseCustom<?> modifyProfile(@RequestBody PatchProfileReq patchProfileReq,
-                                         @Parameter(hidden = true) @IsLogin LoginStatus loginStatus) {
-    userService.modifyProfile(loginStatus.getUserIdx(), patchProfileReq);
-    return ResponseCustom.OK();
-  }
-
   @ResponseBody
   @PostMapping("/nickname")
   public ResponseCustom<?> checkNickname(@RequestBody PostNicknameReq postNicknameReq) {
     return ResponseCustom.OK(userService.checkNickname(postNicknameReq));
   }
 
-  //유저 탈퇴
-  @Auth
-  @DeleteMapping("/delete")
-  public ResponseCustom<?> deleteUser(
-          @IsLogin LoginStatus loginStatus
-  ) {
-    userService.deleteUser(loginStatus.getUserIdx());
-    return ResponseCustom.OK();
-  }
-
-  //유저 로그아웃
-  @Auth
-  @PostMapping("/logout")
-  public ResponseCustom<?> logout(
-          @IsLogin LoginStatus loginStatus
-  ) {
-    userService.logout(loginStatus.getUserIdx());
-    return ResponseCustom.OK();
-  }
-
-  // 마이페이지 조회
-  @Auth
-  @GetMapping("")
-  public ResponseCustom<MyProfileRes> profile(
-          @IsLogin LoginStatus loginStatus
-  ) {
-    return ResponseCustom.OK(userService.checkProfile(loginStatus.getUserIdx()));
-  }
-
-  //유저 닉네임 검색 조회
   @GetMapping("/search")
   public ResponseCustom<?> searchNickname(
           @Parameter(name = "nickname", description = "닉네임") @RequestParam String nickname
   ) {
     return ResponseCustom.OK(userService.searchNickname(nickname));
-  }
-
-  @Auth
-  @GetMapping("/notification")
-  public ResponseCustom<?> getUserNotification(
-          @IsLogin LoginStatus loginStatus,
-          Pageable pageable
-  ) {
-    return ResponseCustom.OK(userService.getUserNotification(loginStatus.getUserIdx(), pageable));
   }
 
 }

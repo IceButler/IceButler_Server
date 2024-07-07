919d7553cfffb83551907e20db1cc7eead26aceb
sojungpp <sophie2458@naver.com>
2024-07-08 00:40:53 +0900

    #325 refactor: fridge exception 폴더 이동

--- src/main/java/com/example/icebutler_server/fridge/exception/CannotDeleteFridgeException.java
+++ src/main/java/com/example/icebutler_server/fridge/exception/CannotDeleteFridgeException.java
@@ -1,4 +1,4 @@
-package com.example.icebutler_server.user.exception;
+package com.example.icebutler_server.fridge.exception;
 
 public class CannotDeleteFridgeException extends RuntimeException {
     public CannotDeleteFridgeException() { super("해당 냉장고에 멤버가 있어서 삭제할 수 없습니다.");

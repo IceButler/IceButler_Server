a5a7bfe3c83c79d91e54b7fc03167dc39987d421
psyeon1120 <psycsh@naver.com>
2024-07-12 01:25:32 +0900

    #327 feat: ExceptionAdvice 추가

--- src/main/java/com/example/icebutler_server/global/exception/ExceptionAdvice.java
+++ src/main/java/com/example/icebutler_server/global/exception/ExceptionAdvice.java
@@ -3,11 +3,17 @@ package com.example.icebutler_server.global.exception;
 import com.example.icebutler_server.global.dto.response.ResponseCustom;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.sql.SQLException;
+import java.time.format.DateTimeParseException;
 import java.util.Objects;
 
 public class ExceptionAdvice {
@@ -24,5 +30,27 @@ public class ExceptionAdvice {
         FieldError fieldError = Objects.requireNonNull(e.getFieldError());
         ReturnCode returnCode = ReturnCode.findByCode(fieldError.getDefaultMessage());
         return ResponseCustom.error(returnCode);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(
+            value = {
+                    HttpMessageNotReadableException.class,
+                    HttpRequestMethodNotSupportedException.class,
+                    MissingServletRequestParameterException.class,
+                    MethodArgumentTypeMismatchException.class,
+                    DateTimeParseException.class
+            }
+    )
+    protected ResponseCustom handleBaseException(Exception e) {
+        return ResponseCustom.error(ReturnCode.INVALID_PARAM);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(
+            value = {SQLException.class}
+    )
+    protected ResponseCustom handleBaseException(SQLException e) {
+        return ResponseCustom.error(ReturnCode.INTERNAL_SERVER_ERROR);
     }
 }
